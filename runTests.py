20d6021a39b2e2fbe0aabbe5cd355dfb94a7d368
egilad <egilad@switchkey-lm.haifa.corp.yahoo.com>
2016-09-05 17:04:48 +0300

    updated benchmarks script

--- runTests.py
+++ runTests.py
@@ -15,84 +15,100 @@ resultsDir = os.getenv('HOME') + '/results/'
 resultsFile = resultsDir + 'results.txt'
 
 def runName(bench, workload, inits, opers, threads, option):
-        if option != '':
-                option = '+' + option.replace(' ', '_').replace('-', '')
-        return bench + '+' + workload + '+' + inits + '+' + opers + '+' + threads + option
+    if option != '':
+            option = '+' + option.replace(' ', '_').replace('-', '')
+    return bench + '+' + workload + '+' + inits + '+' + opers + '+' + threads + option
 
 def logOutput(name, out):
-        logName = resultsDir + name + '.log'
-        f = open(logName, 'w')
-        f.write(out.decode('utf-8'))
-        f.close()
+    logName = resultsDir + name + '.log'
+    f = open(logName, 'w')
+    f.write(out.decode('utf-8'))
+    f.close()
 
 def getThroughput(name):
-        logName = resultsDir + name + '.log'
-        lre = re.compile(r'Throughput')
-        tre = re.compile(r'[0-9]+')
-        with open(logName) as file:
-                for line in file:
-                        tpline = lre.search(line)
-                        if tpline != None:
-                                throughput = tre.search(line).group()
-                                return throughput
+    logName = resultsDir + name + '.log'
+    tpLineRe = re.compile(r'Throughput')
+    tpValRe = re.compile(r'[0-9]+')
+    tpPutRe = re.compile(r'Mean Insert time')
+    tpGetRe = re.compile(r'Mean Read time')
+    tpLatRe = re.compile(r'[0-9]+\.[0-9]+')
+    throughput = "0"
+    putLat = "0"
+    getLat = "0"
+    with open(logName) as file:
+        for line in file:
+            tpline = tpLineRe.search(line)
+            if tpline != None:
+                throughput = tpValRe.search(line).group()
+            else:
+                putline = tpPutRe.search(line)
+                if putline != None:
+                    putLat = tpLatRe.search(line).group()
+                else:
+                    getline = tpGetRe.search(line)
+                    if getline != None:
+                        getLat = tpLatRe.search(line).group()
+    return (throughput, putLat, getLat)
 
 def recordRun(name, out):
-        ticks = getThroughput(name)
-        resultsLock.acquire()
-        try:
-                f = open(resultsFile, 'a')
-                f.write(name.replace('+', '\t') + '\t' + ticks + '\n')
-                f.close()
-        except Exception as ex:
-                print('*** unable to record {0} result: {1} ticks ***'.format(name, ticks))
-                print('*** reason: {0} ***'.format(str(ex)))
-        resultsLock.release()
+    (throughput, putLat, getLat) = getThroughput(name)
+    resultsLock.acquire()
+    try:
+        f = open(resultsFile, 'a')
+        f.write(name.replace('+', '\t') + '\t' + throughput + '\t' + putLat + '\t' + getLat + '\n')
+        f.close()
+    except Exception as ex:
+        print('*** unable to record {0} result: {1} ticks ***'.format(name, ticks))
+        print('*** reason: {0} ***'.format(str(ex)))
+    resultsLock.release()
 
 def nowStr():
-        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def onExit(out, name):
-        print('\t\tRun {0} completed successfully @ {1}'.format(name, nowStr()))
-        logOutput(name, out)
-        recordRun(name, out)
+    print('\t\tRun {0} completed successfully @ {1}'.format(name, nowStr()))
+    logOutput(name, out)
+    recordRun(name, out)
 
 def onError(ex, name):
-        print('-\t-\tRun {0} failed ({1}) @ {2}'.format(name, ex.returncode, nowStr()))
-        logOutput(name, ex.output)
+    print('-\t-\tRun {0} failed ({1}) @ {2}'.format(name, ex.returncode, nowStr()))
+    logOutput(name, ex.output)
 
 def deleteFiles():
-        print("deleting data files... ", end="", flush=True)
-        [os.remove("./data/" + f) for f in os.listdir("./data")]
-        print("done")
+    print("deleting data files... ", end="", flush=True)
+    [os.remove("./data/" + f) for f in os.listdir("./data")]
+    print("done")
 
 def popenAndCall(bench, workload, inits, opers, threads, opt):
-        name = runName(bench, workload, inits, opers, threads, opt)
-        try:
+    name = runName(bench, workload, inits, opers, threads, opt)
+    try:
 #               cmd = './ycsbc -db {0} -P ./workloads/{1}.spec -threads {2} -recordcount {3} -operationcount {4} {5}' \
 #                       .format(bench, workload, threads, inits, opers, opt)
-                cmd = './ycsbc -db {0} -P ../piwi/workloads/{1}.spec -threads {2} -operationcount {3} {4}' \
-                        .format(bench, workload, threads, opers, opt) # inits omitted! (set in benchmark file)
-                print('\t\tStarting: ' + cmd)
-                out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-                onExit(out, name)
-        except subprocess.CalledProcessError as ex:
-                onError(ex, name)
-        deleteFiles()
-        return
+        cmd = './ycsbc -db {0} -P ../piwi/workloads/{1}.spec -threads {2} -operationcount {3} {4}' \
+                .format(bench, workload, threads, opers, opt) # inits omitted! (set in benchmark file)
+        print('\t\tStarting: ' + cmd)
+        out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+        onExit(out, name)
+    except subprocess.CalledProcessError as ex:
+        onError(ex, name)
+    deleteFiles()
+    return
 
 
 f = open(resultsFile, 'a')
-f.write('benchmark\tworkload\tinitNum\topersNum\tthreadsNum\trun_options\tops_per_sec\n')
+f.write('benchmark\tworkload\tinitNum\topersNum\tthreadsNum\trun_options\tops_per_sec\tputLat\tgetLat\n')
 f.close()
 
 #benchmarks = ['piwi', 'rocks']
 benchmarks = ['piwi']
-workloads = ['gets_from_keystore', 'gets_from_writebuf', 'puts_and_rebalance', 'puts_gets_rebalances']
-initsNum = ['varies']
-opersNum =  ['10000']
-threadsNum = ['1', '4', '16']
+#workloads = ['gets_from_keystore', 'gets_from_writebuf', 'puts_and_rebalance', 'puts_gets_rebalances']
+workloads = ['puts_gets_rebalances']
+initsNum = ['10000']
+opersNum =  ['1000000']
+threadsNum = ['1', '2', '4', '8', '16']
 #options = [''] # must have at least one option, possibly empty
-options = ['-initChunks 1', '-initChunks 10', '-initChunks 100']
+commonOps = '-initChunks 100'
+options = [commonOps + ' -workerThreadsNum 0', commonOps + ' -workerThreadsNum 1', commonOps + ' -workerThreadsNum 2', commonOps + ' -workerThreadsNum 4']
 
 # note: on the zipfian distribution, the range of values used to generate keys is initsNum + opersNum * rateOfInsert * 2
 # also, default value size is 100 chars (probably configurable via fieldlength, not sure)
