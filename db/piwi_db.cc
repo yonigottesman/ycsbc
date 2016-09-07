9a4a951139171720f18130540f7e0338cde4ad88
egilad <egilad@switchkey-lm.haifa.corp.yahoo.com>
2016-09-07 16:37:01 +0300

    added scan support, untested yet

--- db/piwi_db.cc
+++ db/piwi_db.cc
@@ -32,13 +32,30 @@ int PiwiDB::Insert(const string& table, const string& key,
 	return DB::kOK;
 }
 
-int PiwiDB::Read(const std::string &table, const std::string &key,
-			const std::vector<std::string> *fields, std::vector<KVPair> &result)
+int PiwiDB::Read(const string &table, const string &key,
+			const vector<string> *fields, vector<KVPair> &result)
 {
 	string value;
 	piwi::get(key, value);
 	result.push_back(KVPair("", value));
 	return DB::kOK;
 }
+
+int PiwiDB::Scan(const string &table, const string &from, int record_count,
+		const vector<string> *fields,
+		vector<vector<KVPair>> &result)
+{
+	const char* fromNum = from.c_str() + from.size() - 9; // 9 = digits in int
+	int num = stoi(fromNum);
+	num += record_count;
+	string to = from.substr(0, from.size() - 9);
+	to += to_string(num);
+	list<KVPair> resultList;
+	piwi::scan(from, to, resultList);
+	// avoiding the copy from list to vector for now - it's pointless and wasteful
+	result[0].emplace_back(from, to);
+	return DB::kOK;
+}
+
 
 } /* namespace ycsbc */
